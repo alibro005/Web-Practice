ff45ea960ada850e41a8ea5225c18af238941d38
alibro005 <itsalibro9@gmail.com>
2025-08-20 12:55:06 +0500

    update the project Nexcent

--- Project/index.js
+++ Project/index.js
@@ -1,6 +1,33 @@
-const navMenu = document.getElementById("nav-items");
-const hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener("click",function(){
-     navMenu.classList.toggle('show');
-     navMenu.classList.add('hide');
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
 });
+
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
